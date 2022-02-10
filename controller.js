3394242323242f81be3362c00d1c0b133b9b9175
jebzmos4 <jebzmos4@gmail.com>
2022-02-10 19:24:56 +0100

    modified response message

--- controller.js
+++ controller.js
@@ -9,7 +9,52 @@ exports.health = async function(req, res, next) {
 exports.messages = async function(req, res, next) {
     try {
         console.log("===>", req.body)
-        res.status(200).send('Welcome. How are you doing?');
+        res.status(200).json({
+            "blocks": [
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Welcome. How are you doing?",
+                        "emoji": true
+                    }
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "emoji": true,
+                                "text": "Doing Well"
+                            },
+                            "style": "primary",
+                            "value": "primary"
+                        },
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "emoji": true,
+                                "text": "Neutral"
+                            },
+                            "value": "neutral"
+                        },
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "emoji": true,
+                                "text": "Feeling Lucky"
+                            },
+                            "style": "danger",
+                            "value": "lucky"
+                        }
+                    ]
+                }
+            ]
+        });
     } catch (e) {
         next(e)
     }
