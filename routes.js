8765e2079e1c2c9ad7689cf40ccd932709133de2
jebzmos4 <jebzmos4@gmail.com>
2022-02-10 18:40:04 +0100

    fixed route verb issue

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Controller = require('./controller');
 
 router.get('/health', Controller.health);
-router.get('/messages', Controller.messages);
+router.post('/messages', Controller.messages);
 router.get('/callback', Controller.callback);
 
 module.exports = function(app, prefix = '') {
